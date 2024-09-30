26518b0e222547ac5434a2fdaf97d77c66f85448
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-10-01 00:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const  input1=document.querySelector("#text")
+const input2=document.querySelector("#delay")
+const button=document.querySelector("#btn")
+
+
+	async function print(){
+	const text=input1.value
+		const delay=input2.value
+	
+	 document.getElementById("output").textContent
+	await new Promise((resolve) => setTimeout(resolve, delay));
+
+        
+        document.getElementById("output").textContent = text;
+	}
+button.addEventListener("click", print);
